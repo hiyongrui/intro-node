2d6db057b3c652edc516a100e15baabc2cdb713c
gowhr <yongrui.tan.2022@scis.smu.edu.sg>
2023-10-13 16:46:37 +0800

    old way of require, module.exports vs new way of import

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import { count } from "./utils.js";
-import whateverdefault from ".utils.js"
+const {count} = require('.utils.js')
+
+import fs from 'node:fs' //explicit saying fs is from internal node, not 3rd party installed package
+import _ from 'lodash' // 3rd party installed package
 
 const note = process.argv[2]; // first arg node environment, second this file
 
--- utils.js
+++ utils.js
@@ -1,3 +1,9 @@
 export const count = num => num
 
+exports.count = num => num
+
+module.exports = {
+    count
+}
+
 export default {name: 'cookies'}
